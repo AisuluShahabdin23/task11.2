5b8ed4df710762647e2797318901431d4e32b98f
Aisulu <aisulu_sh_98@mail.ru>
2023-07-18 00:29:54 +0600

    Create function transform in file str_func.py

--- str_func.py
+++ str_func.py
@@ -4,11 +4,3 @@ def transformation(input_string):
     """
     input_string = input()
     return input_string.upper()
-
-
-def transformation1(input_string):
-    """
-    Делает заглавными первые буквы каждого слова в строке
-    """
-    input_string = input()
-    return input_string.title()
