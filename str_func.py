3924efacc94234b0601c585524982998d7d535b8
AisuluShahabdin23 <139695265+AisuluShahabdin23@users.noreply.github.com>
2023-07-18 21:23:30 +0600

    Update str_func.py

--- str_func.py
+++ str_func.py
@@ -2,5 +2,6 @@ def transformation(input_string):
     """
     Делает заглавными первые буквы каждого слова в строке
     """
+    print(1+2)
     input_string = input()
     return input_string.upper()
