4a16c26887b9c4b7be249ec8a24493daf732a17f
Aisulu <aisulu_sh_98@mail.ru>
2023-07-18 21:35:10 +0600

    test

--- str_func.py
+++ str_func.py
@@ -2,6 +2,5 @@ def transformation(input_string):
     """
     Делает заглавными первые буквы каждого слова в строке
     """
-    print(1+2)
     input_string = input()
     return input_string.upper()
