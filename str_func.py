c9d166fd1df2594a7f2d959438512eda7b0de9cc
Aisulu <aisulu_sh_98@mail.ru>
2023-07-18 20:40:30 +0600

    Create new_function

--- str_func.py
+++ str_func.py
@@ -1,3 +1,11 @@
 def transformation(input_string):
     input_string = input()
     return input_string.upper()
+
+
+def transformation1(input_string):
+    """
+    Делает заглавными первые буквы каждого слова в строке
+    """
+    input_string = input()
+    return input_string.title()
