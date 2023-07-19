84e8b8677ea8ab3b2556abe25c3a7a3d1e44440c
AisuluShahabdin23 <139695265+AisuluShahabdin23@users.noreply.github.com>
2023-07-19 21:17:19 +0600

    Update str_func.py

--- str_func.py
+++ str_func.py
@@ -1,6 +1,6 @@
 def transformation(input_string):
     """
-    Возвращает строку заглавными буквами
+    возвращает строку заглавными буквами
     """
     input_string = input()
     return input_string.upper()
